c9e9005bba4101c4db3d0a1826ed3aee6a1cf9c4
Anthony Guerrero <krezket@Anthonys-Mac-Studio.local>
2024-04-13 18:33:17 -0700

    added COntact page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,6 +10,7 @@ export default function Header() {
           <li><Link to="/">Home</Link></li>
           <li><Link to="/about">About</Link></li>
           <li><Link to="/services">Services</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
         </ul>
       </nav>
     </header>
---
+++ src/pages/Contact/Contact.jsx
@@ -0,0 +1,13 @@
+import Header from "../../components/Header/Header";
+
+export default function Contact() {
+  return (
+    <>
+        <Header />
+        <main>
+            <h1>Contact</h1>
+            <p>Welcome to the Contact page!</p>
+        </main>
+    </>
+  )
+}
